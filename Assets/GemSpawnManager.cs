faee2d51d5a2c1a2ad98ce91469fb819038c2fbb
DavidBjorkberg <david.bjorkberg1@gmail.com>
2021-07-09 23:22:25 +0200

    Gems spawn randomly, Player can pick up gem, it gets added to body

---
+++ Assets/GemSpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawnManager : MonoBehaviour
+{
+    [SerializeField] int gemSpawnDelayInSteps;
+    [SerializeField] Gem gemPrefab;
+    GridManager gridManager;
+    int gemSpawnTimer;
+    private void Awake()
+    {
+        gridManager = GetComponent<GridManager>();
+    }
+    void Start()
+    {
+    }
+    public void Step()
+    {
+        gemSpawnTimer--;
+        if (gemSpawnTimer <= 0)
+        {
+            SpawnGem();
+            gemSpawnTimer = gemSpawnDelayInSteps;
+        }
+    }
+    void SpawnGem()
+    {
+        GridManager.CellInfo randomCell = gridManager.GetRandomFreeCell();
+
+        Gem spawnedGem = Instantiate(gemPrefab, randomCell.pos, Quaternion.identity);
+        gridManager.AddEntityToCell(spawnedGem, randomCell.index);
+    }
+}
