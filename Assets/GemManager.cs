3885e17bbfaad49bc63992c89519974c5ea819bf
DavidBjorkberg <david.bjorkberg1@gmail.com>
2021-07-10 14:04:30 +0200

    Several kinds of gems can spawn and are added to the body when eaten. Gems have animation

---
+++ Assets/GemManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemManager : MonoBehaviour
+{
+    public enum GemType { Ruby, Amber, Topaz, Emerald, Sapphire, Amethyst, Diamond, _MAX };
+    [SerializeField] int gemSpawnDelayInSteps;
+    [SerializeField] Gem gemPrefab;
+    [SerializeField] List<RuntimeAnimatorController> animControllers;
+    GridManager gridManager;
+    int gemSpawnTimer;
+    private void Awake()
+    {
+        gridManager = GetComponent<GridManager>();
+    }
+    public void Step()
+    {
+        gemSpawnTimer--;
+        if (gemSpawnTimer <= 0)
+        {
+            SpawnGem();
+            gemSpawnTimer = gemSpawnDelayInSteps;
+        }
+    }
+    void SpawnGem()
+    {
+        GridManager.CellInfo randomCell = gridManager.GetRandomFreeCell();
+
+        Gem spawnedGem = Instantiate(gemPrefab, randomCell.pos, Quaternion.identity);
+        spawnedGem.InitializeGem(GetGemTypeToSpawn());
+        gridManager.AddEntityToCell(spawnedGem, randomCell.index);
+    }
+    public GemType GetGemTypeToSpawn()
+    {
+        int randomGem = Random.Range(0, (int)GemType._MAX);
+
+        return (GemType)randomGem;
+    }
+    public RuntimeAnimatorController GetGemAnimController(GemType gemType)
+    {
+        return animControllers[(int)gemType];
+    }
+}
